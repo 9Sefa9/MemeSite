b29f2dfe09ba25cf94e110deee57362afed9c4b8
9schurke9 <sefagoevercin@hotmail.de>
2021-01-25 03:04:23 +0100

    Meme Site is alive!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Memes from './Memes'
+function App() {
+  return (
+    <div className="App">
+      <h1>MEME</h1>
+      <h3>Less Talking - Download Your Desired Meme </h3>
+       <Memes /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Memes.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+function Memes() {
+    const [state,setState] = useState({fetchedMemes:[]});
+    useEffect(()=>{
+        async function getMemes(){
+            try{
+                let m = await fetch('https://api.imgflip.com/get_memes');
+                let d = await m.json();
+                setState({fetchedMemes:d.data.memes});
+        
+            }catch(err){
+                console.log('error!:',err);
+            }
+        }
+        getMemes();
+    },[]);
+
+    return (
+        <ul className="meme-container">            
+           {state.fetchedMemes.map(element => {return <li key={element.id}><p><strong>{element.name}</strong></p><img alt='a wonderful meme'src={element.url}/></li> })
+            }
+        </ul>  
+    );
+}
+
+export default Memes;
