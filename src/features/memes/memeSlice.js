1a98abed566d27fd7a4db19c0cf6c7deabcd4e1f
9Sefa9 <sefagoevercin@hotmail.de>
2021-11-02 19:26:26 +0100

    ..

--- src/features/memes/memeSlice.js
+++ src/features/memes/memeSlice.js
@@ -1,1 +1,23 @@
+/* A function that accepts an initial state, 
+*  an object of reducer functions, and a "slice name", and automatically generates action creators 
+*  and action types that correspond to the reducers and state.
+*/
 import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    value: 0,
+    stateName = "init"
+};
+
+const memeSlice = createSlice({
+    name: 'meme',
+    initialState,
+    reducers:{
+        fetchMemes(state){
+            
+        }
+    }
+});
+
+export const {fetchMemes} = memeSlice.actions;
+export default memeSlice.reducer;
