9d8d803d1ed03d6698113538853a4f1d185ded49
9Sefa9 <sefagoevercin@hotmail.de>
2021-11-03 15:03:37 +0100

    prod build

--- src/features/memes/Memes.js
+++ src/features/memes/Memes.js
@@ -36,7 +36,7 @@ function Memes() {
         
     },[]);
 
-    return !state.fetchedMemes ? (
+    return state.fetchedMemes ? (
         <ul className="meme-container">            
            {state.fetchedMemes.map(element => {return <li key={element.id}><p><strong>{element.name}</strong></p><img alt='a wonderful meme'src={element.url}/></li> })
             }
